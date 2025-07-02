72feec05c20726ee8dbad7015d8d61f5e1447d33
MJCarvajalQ <144226562+MJCarvajalQ@users.noreply.github.com>
2025-07-02 13:10:52 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/mjcarvajalq/sales_metrics_api/SalesMetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mjcarvajalq.sales_metrics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesMetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesMetricsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjcarvajalq/sales_metrics_api/SalesMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjcarvajalq.sales_metrics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesMetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
